28f39aa5417cd124f2628a17af1eb90d95c17f3b
nurgisa <apbaz.nurgisa@gmail.com>
2024-01-07 20:00:15 +0500

    hometasks

--- 2/4-more-examples.php
+++ 2/4-more-examples.php
@@ -1,8 +1,10 @@
 <?php
 
-
+// example 1
+// определение времени года по месяцу
 $month = "January";
 
+// с помощью switch
 switch ($month) {
     case "January":
     case "February":
@@ -28,7 +30,7 @@ switch ($month) {
         echo "Invalid month.";
 }
 
-
+// с помощью if
 if ($month == "December" || $month == "January" || $month == "February") {
     echo "It's winter.";
 } elseif ($month == "March" || $month == "April" || $month == "May") {
@@ -40,6 +42,7 @@ if ($month == "December" || $month == "January" || $month == "February") {
 }
 
 
+// с помощью if и in_array()
 if (in_array($month, ["December", "January", "February"])) {
     echo "It's winter.";
 } elseif (in_array($month, ["March", "April", "May"])) {
@@ -51,3 +54,41 @@ if (in_array($month, ["December", "January", "February"])) {
 }
 
 
+// example 2
+// приветствие пользователя
+
+$is_authorized = true;
+$is_admin = false;
+
+if ($is_authorized) {
+    if ($is_admin) {
+        echo "Hello, admin!";
+    } else {
+        echo "Hello, user!";
+    }
+} else {
+    echo "Hello, guest!";
+}
+
+
+// example 3
+// определение типа переменной
+$var = 43;
+
+if (is_string($var)) {
+    echo "It's a string.";
+} elseif (is_int($var)) {
+    echo "It's an integer.";
+} elseif (is_float($var)) {
+    echo "It's a float.";
+} elseif (is_bool($var)) {
+    echo "It's a boolean.";
+} elseif (is_array($var)) {
+    echo "It's an array.";
+} elseif (is_object($var)) {
+    echo "It's an object.";
+} elseif (is_null($var)) {
+    echo "It's null.";
+} else {
+    echo "It's something else.";
+}
