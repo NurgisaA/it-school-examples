37592626ce1fd9ec7454d61ee61ad1f434928861
nurgisa <apbaz.nurgisa@gmail.com>
2024-01-08 19:03:32 +0500

    hometasks

--- 2/hometask/leap-year.php
+++ 2/hometask/leap-year.php
@@ -1,4 +1,4 @@
 <?php
-
+// https://ru.wikihow.com/%D0%B2%D1%8B%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%D1%81%D0%BD%D1%8B%D0%B5-%D0%B3%D0%BE%D0%B4%D1%8B
 // https://www.wikihow.com/images/thumb/0/0d/Calculate-Leap-Years-Step-2-Version-2.jpg/v4-728px-Calculate-Leap-Years-Step-2-Version-2.jpg
 // Написать программу, которая определяет, является ли год високосным.
